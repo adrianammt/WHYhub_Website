8fd368e62ce4328f8ebd9ca55e1916ac0dbacdab
Adriana Morales <amorat@gmx.de>
2021-10-04 22:57:20 +0200

    Import header, main and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <div className="App">WHYHub Website</div>;
-  
+  return <div className="App">
+  <Header/>  
+  <Main/>
+  <Footer/>
+  </div>;
 }
 
 export default App;
