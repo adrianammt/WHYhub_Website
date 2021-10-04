50a12f854b9a08597cf2e6a63da77d2148f5e2f0
Adriana Morales <amorat@gmx.de>
2021-10-04 23:32:42 +0200

    Add Logo to Header and basic css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import { NavLink } from "react-router-dom";
+import Logo from "../assets/WhyHubLogoA1.png"
+import "./Header.css"
 
 export default function Header(){
     return(
-    <nav>
-        <NavLink to="/home">Logo</NavLink>
-        <NavLink to="/QuienesSomos">Quienes somos</NavLink>
-        <NavLink to="/QueHacemos">Que hacemos</NavLink>
-        <NavLink to="/Contacto">Contacto</NavLink>
+    <nav className="HeaderNav">
+        <NavLink to="/home" className="NavLink"><img src={Logo} alt="WhyHub Logo" className="Logo"/></NavLink>
+        <NavLink to="/QuienesSomos" className="NavLink">Quienes somos</NavLink>
+        <NavLink to="/QueHacemos" className="NavLink">Que hacemos</NavLink>
+        <NavLink to="/Contacto" className="NavLink">Contacto</NavLink>
     </nav>
     )
 }
