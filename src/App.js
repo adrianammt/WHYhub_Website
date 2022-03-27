5c58f6528fab658725621721be5df857dbdea3b8
Adriana Morales <amorat@gmx.de>
2022-03-27 18:36:57 +0200

    Change in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
@@ -7,7 +6,6 @@ function App() {
   return <div className="App">
   <Header/>  
   <Main/>
-  <Footer/>
   </div>;
 }
 
