0e9b5c4e8b8eba47d1ddf46d4ba834ab4be7d664
Adriana Morales <amorat@gmx.de>
2022-03-27 18:34:21 +0200

    Footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
+import "./Footer.css"
+import { NavLink } from "react-router-dom";
+import Logo from "../assets/WhyHubLogoA1.png"
+
 export default function Footer(){
     return(
-        <section>
-            <div>Logo</div>
+        <section className="FooterNav">
+            <NavLink to="/home" className="NavLink"><img src={Logo} alt="WhyHub Logo" className="Logo"/></NavLink>
             <div>Menu</div>
             <div>Redes Sociales</div>
         </section>
