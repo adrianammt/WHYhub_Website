30ddc58f4aecfa2e34297189c306a69818401172
Adriana Morales <amorat@gmx.de>
2022-03-27 22:38:47 +0200

    Agregar seccion y cambios de texto en pagina quienes somos

--- src/pages/QuienesSomos.js
+++ src/pages/QuienesSomos.js
@@ -8,18 +8,27 @@ export default function QuienesSomos(){
     <section className="Heroshot">
     <div className="Heroshot-text">
     <h1 className="Tagline">Innovación al servicio de la comunidad</h1>
-    <p className="Descripcion">The Why Hub ayuda a las organizaciones a maximizar su impacto en la sociedad, contruyendo con ellas soluciones centradas en los y las usuarias de los servicios que entregan.</p>
-    <p className="Descripcion">Partimos de la premisa que son las personas quienes tienen las soluciones a sus propios problemas y cómo cientificos sociales ayudamos a construir un camino que nos ayude a incorporarlas en el diseño, implementación y evaluación de los servicios dirigidos a los y las ciudadanas.</p>
+    <p className="Descripcion">Somos una comunidad de investigadores y gestores que desarrollamos diseños centrados en el usuario, investigamos y evaluamos proyectos del sector social y humanitario.</p>
     </div>
     <div className="ImagenInicio">The truth is human</div>
     </section>
     <Separador
-    texto="&quot;Lorem lorem lorem&quot;"/>
-    <section className="Mision">
-        <h2 className="Title">Nuestra Misión</h2>
-        <p className="Descripcion">Potenciar el impacto que tienen las organizaciones sociales en Latinoamerica. Queremos que cada recurso humano, monetario y de tiempo invertido mejore la vida de en la población mas vulnerable de la región.</p>
+    texto="&quot;Preguntamos para co-crear soluciones centradas en las personas.&quot;"/>
+        <section className="Contenido">
+        <h2 className="Title">Historia - WHY we came to be</h2>
+        <p className="Descripcion">Los problemas sociales son complejos y para resolverlos necesitamos hacer preguntas desde distintas perspectivas, desde distintas disciplinas y públicos (investigadores/as, gestores/as, ciudadanas/os, entre otros).  
+Necesitamos hacer preguntas que rompan la inercia de las soluciones comunes.  Latinoamérica ha cambiado pero seguimos haciendo las mismas preguntas y aplicando las mismas respuestas que hace 40 años.</p>
     </section>
-    <Separador/>
+    <Separador  texto="&quot;Lorem lorem lorem lorem&quot;"/>
+    <section className="Contenido">
+        <h2 className="Title">Nuestra Misión</h2>
+        <p className="Descripcion">Potenciar el impacto que tienen las organizaciones sociales en Latinoamerica.</p>
+        <ul>
+        <li>Queremos cristalizar la utopía de un presente más empático, mas solidario, mas colectivo: más humano.</li>
+        <li>Queremos encontrar soluciones innovadoras a los problemas sociales que se atienden desde las organizaciones privadas y públicas, creando una comunidad reflexiva y multidisciplinaria.</li>
+        <li>Queremos crear una comunidad donde cada investigador o gestor mire e interpele la realidad desde distintas perspectivas y haciéndole preguntas disonantes.</li>
+        </ul>
+    </section>
     <Footer/>
     </>
     )
